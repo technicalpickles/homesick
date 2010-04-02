c50464e366e26d00204a3b84f86ce9ba02589aeb
Joshua Nichols <josh@technicalpickles.com>
2010-04-01 20:16:21 -0400

    Added more clone specs.

--- spec/homesick_spec.rb
+++ spec/homesick_spec.rb
@@ -5,15 +5,29 @@ describe Homesick do
     @homesick = Homesick.new
   end
 
-  it "should clone any git repo" do
-    @homesick.should_receive(:git_clone).with('git://github.com/technicalpickles/pickled-vim.git')
+  describe "clone" do
+    it "should clone git repo like git://host/path/to.git" do
+      @homesick.should_receive(:git_clone).with('git://github.com/technicalpickles/pickled-vim.git')
 
-    @homesick.clone "git://github.com/technicalpickles/pickled-vim.git"
-  end
+      @homesick.clone "git://github.com/technicalpickles/pickled-vim.git"
+    end
 
-  it "should clone a github repo" do
-    @homesick.should_receive(:git_clone).with('git://github.com/wfarr/dotfiles.git', :destination => 'wfarr_dotfiles')
+    it "should clone git repo like git@host:path/to.git" do
+      @homesick.should_receive(:git_clone).with('git@github.com:technicalpickles/pickled-vim.git')
 
-    @homesick.clone "wfarr/dotfiles"
+      @homesick.clone 'git@github.com:technicalpickles/pickled-vim.git'
+    end
+
+    it "should clone git repo like http://host/path/to.git" do
+      @homesick.should_receive(:git_clone).with('http://github.com/technicalpickles/pickled-vim.git')
+
+      @homesick.clone 'http://github.com/technicalpickles/pickled-vim.git'
+    end
+
+    it "should clone a github repo" do
+      @homesick.should_receive(:git_clone).with('git://github.com/wfarr/dotfiles.git', :destination => 'wfarr_dotfiles')
+
+      @homesick.clone "wfarr/dotfiles"
+    end
   end
 end
