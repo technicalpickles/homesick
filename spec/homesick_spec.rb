b1f2742422721de9bf4823a147c756b345bcd892
muratayusuke <info@muratayusuke.com>
2013-06-19 15:30:18 +0000

    fix coding style

--- spec/homesick_spec.rb
+++ spec/homesick_spec.rb
@@ -149,7 +149,7 @@ describe "homesick" do
         homesick.symlink("glencairn")
 
         home_dotdir = home.join(".config")
-        home_dotdir.symlink?.should == false
+        home_dotdir.symlink?.should be == false
         home_dotdir.join(".some_dotfile").readlink.should == dotfile
       end
     end
@@ -163,7 +163,7 @@ describe "homesick" do
         homesick.symlink("glencairn")
 
         home_dotdir = home.join(".config").join("appA")
-        home_dotdir.symlink?.should == false
+        home_dotdir.symlink?.should be == false
         home_dotdir.join(".some_dotfile").readlink.should == dotfile
       end
     end
@@ -180,9 +180,9 @@ describe "homesick" do
 
         home_config_dir = home.join(".config")
         home_appA_dir = home_config_dir.join("appA")
-        home_config_dir.symlink?.should == false
-        home_config_dir.join(".some_dotfile").readlink.should == config_dotfile
-        home_appA_dir.symlink?.should == false
+        home_config_dir.symlink?.should be == false
+        home_config_dir.join(".some_dotfile").readlink.should be == config_dotfile
+        home_appA_dir.symlink?.should be == false
         home_appA_dir.join(".some_appfile").readlink.should == appA_dotfile
       end
     end
