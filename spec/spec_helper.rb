0a9cefc3bea69b93b4497bbb2cc5f4f5fcd1642a
Nicolas McCurdy <thenickperson@gmail.com>
2014-01-06 01:18:08 -0500

    Remove some extra file loading code that "bundle gem" doesn't seem to need

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,6 @@
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'homesick'
-require 'rspec'
-require 'rspec/autorun'
 require 'construct'
-require 'tempfile'
 
 RSpec.configure do |config|
   config.include Construct::Helpers
