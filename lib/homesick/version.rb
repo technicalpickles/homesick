b60703d496df464131f7b9cdb4fc32aef005d127
Jeremy Cook <jeremycook0@gmail.com>
2014-04-18 10:47:58 -0400

    Bumped version number in preparation for a release.

--- lib/homesick/version.rb
+++ lib/homesick/version.rb
@@ -4,9 +4,9 @@ module Homesick
   # String of the entire version number
   module Version
     MAJOR = 1
-    MINOR = 0
+    MINOR = 1
     PATCH = 0
 
-    STRING = "#{MAJOR}.#{MINOR}.#{PATCH}"
+    STRING = [MAJOR, MINOR, PATCH].compact.join('.')
   end
 end
